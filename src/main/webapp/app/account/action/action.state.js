e17c463510a72177511ae95f78e7f055d9cbea63
Ahmed Ben Ouezdou <ahmed.ben-ouezdou@ggv.fr>
2017-08-28 18:12:15 +0200

    update

---
+++ src/main/webapp/app/account/action/action.state.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('appApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('action', {
+            parent: 'account',
+            url: '/action',
+            data: {
+                authorities: ['ROLE_USER'],
+                pageTitle: 'global.menu.account.settings'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/account/action/action.html',
+                    controller: 'ActionController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('settings');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
