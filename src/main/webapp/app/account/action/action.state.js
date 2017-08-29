94a1fd0b31a7a7099400aff37f15d9c9fae6a7f9
Ahmed ben ouezdou <ahmed.b.ouezdou@gmail.com>
2017-08-30 00:11:19 +0200

    update

--- src/main/webapp/app/account/action/action.state.js
+++ src/main/webapp/app/account/action/action.state.js
@@ -28,6 +28,43 @@
                     return $translate.refresh();
                 }]
             }
+        }).state('bye', {
+            parent: 'action',
+            url: '/bye',
+            data: {
+                authorities: ['ROLE_USER']
+            },
+            views: {
+                'detail': {
+                    templateUrl: 'app/account/action/byeToken/byeToken.html'
+
+                                  }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('settings');
+                    return $translate.refresh();
+                }]
+            }
+        }).state('invest', {
+            parent: 'action',
+            url: '/invest',
+            data: {
+                authorities: ['ROLE_USER']
+            },
+            views: {
+
+                'detail': {
+                    templateUrl: 'app/account/action/howInvest/howInvest.html'
+
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('settings');
+                    return $translate.refresh();
+                }]
+            }
         });
     }
 })();
